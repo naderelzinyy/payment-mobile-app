de598e84e9d1ebb2c0a25b55e787c1c55f079cac
Nader Elziny <naderelziny805@gmail.com>
2023-01-12 14:59:37 +0300

    Added main_button ui.

---
+++ lib/custom_widgets/main_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:payment_app/component/colors.dart';
+
+class MainButton extends StatelessWidget {
+  final Color? backgroundColor;
+  final Color? textColor;
+  final Function()? onTap;
+  final bool? isBorder;
+  final String text;
+
+  const MainButton({
+    Key? key,
+    this.backgroundColor,
+    this.onTap,
+    this.textColor,
+    required this.text,
+    this.isBorder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              width: 1.5,
+              color: AppColor.selectColor,
+            )),
+        child: const Text(
+          'Pay',
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
