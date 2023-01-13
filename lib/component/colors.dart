366e7989dab97d5f6ed6d1e85d80abf44a8d85be
Nader Elziny <naderelziny805@gmail.com>
2023-01-13 04:16:53 +0300

    Removed the const modifier

--- lib/component/colors.dart
+++ lib/component/colors.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 
 class AppColor {
   static Color backGroundColor = Color(0xFFecf1f4);
-  static const Color mainColor = Color(0xFF192c49);
+  static Color mainColor = Color(0xFF192c49);
   static Color green = Color(0xFF73be93);
   static Color idColor = Color(0xFF9ca2ac);
   static Color selectBackground = Color(0xFFf1f4f8);
