f65bc796a0afbc5642df9cbd5012c700c7c4adf0
Nader Elziny <naderelziny805@gmail.com>
2023-01-15 00:27:19 +0300

    Added two methods (index and submit) as (get and post).

---
+++ payment_backend/app/Http/Controllers/BillInfoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\BillInfo;
+
+class BillInfoController extends Controller
+{
+    //
+    public function index(){
+        $info = BillInfo::first();
+        return json_decode($info);
+    }
+
+    public function submit(){
+        $info = new BillInfo;
+        $info->brand_name = $request->post("brand_name");
+        $info->brand_id = $request->post("brand_id");
+        $info->price = $request->post("price");
+        $info->message = $request->post("message");
+        if($info->save()){
+            return response()->json([
+                "code" => -1,
+                "msg" => "success",
+            ]);
+        }
+        return response()->json([
+            "code" => -1,
+            "message" => "fail",
+        ]);
+
+    }
+}
--- payment_backend/routes/api.php
+++ payment_backend/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/billinfo',[App\Http\Controllers\BillInfoController::class, 'index']);
+Route::post('/submit',[App\Http\Controllers\BillInfoController::class, 'submit']);
