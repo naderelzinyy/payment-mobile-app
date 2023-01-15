87923d1463ac9252e175179b5ab5ccf0d454d17e
Nader Elziny <naderelziny805@gmail.com>
2023-01-15 05:07:45 +0300

    Fixed request issues in submit function.

--- payment_backend/app/Http/Controllers/BillInfoController.php
+++ payment_backend/app/Http/Controllers/BillInfoController.php
@@ -13,15 +13,16 @@ class BillInfoController extends Controller
         return json_decode($info);
     }
 
-    public function submit(){
+    public function submit(Request $request){
         $info = new BillInfo;
         $info->brand_name = $request->post("brand_name");
         $info->brand_id = $request->post("brand_id");
         $info->price = $request->post("price");
+        $info->status = $request->post("status");
         $info->message = $request->post("message");
         if($info->save()){
             return response()->json([
-                "code" => -1,
+                "code" => 1,
                 "msg" => "success",
             ]);
         }
