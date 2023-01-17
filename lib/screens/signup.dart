34a78e505c0ec242b20e44ff313ca7fff601b6e6
Nader Elziny <naderelziny805@gmail.com>
2023-01-17 10:12:22 +0300

    Added signup page.

---
+++ lib/screens/signup.dart
@@ -0,0 +1,143 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:payment_app/Services/authentication.dart';
+import 'package:payment_app/custom_widgets/globals.dart';
+
+import '/custom_widgets/rounded_button.dart';
+import '../component/colors.dart';
+import 'home.dart';
+import 'login.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  String _email = '';
+  String _password = '';
+  String _first_name = '';
+  String _last_name = '';
+
+  createAccountPressed() async {
+    bool emailValid = RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(_email);
+    if (emailValid) {
+      http.Response response = await AuthServices.register(
+          _first_name, _last_name, _email, _password);
+      Map responseMap = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => const HomeScreen(),
+            ));
+      } else {
+        errorSnackBar(context, responseMap.values.first[0]);
+      }
+    } else {
+      errorSnackBar(context, 'Invalid email');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.mainColor,
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          'Registration',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'First Name',
+              ),
+              onChanged: (value) {
+                _first_name = value;
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Last Name',
+              ),
+              onChanged: (value) {
+                _last_name = value;
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Email',
+              ),
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+              ),
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            RoundedButton(
+              btnText: 'Create Account',
+              onBtnPressed: () => createAccountPressed(),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const LoginScreen(),
+                    ));
+              },
+              child: const Text(
+                'already have an account',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
