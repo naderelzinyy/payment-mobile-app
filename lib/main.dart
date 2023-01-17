a21d83e8e48f4f3b53d593e5f1133be1cd1d7bdd
Nader Elziny <naderelziny805@gmail.com>
2023-01-17 10:15:24 +0300

    Major refactor in the main pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:payment_app/screens/home.dart';
+import 'package:payment_app/screens/initial.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const InitialScreen(),
     );
   }
 }
